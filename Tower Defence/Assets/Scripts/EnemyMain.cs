9c983f343cfb6eafa8b4a3ddbacf82a2b6f9556d
Alexbroking <alexbroking@gmail.com>
2018-10-29 13:42:46 +0100

    SecondCommmit

---
+++ Tower Defence/Assets/Scripts/EnemyMain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMain : MonoBehaviour
+{
+
+   // Sprite[] sprites = Resources.LoadAll<Sprite>(pathToTexture);
+
+    private enum Enemies { Goblin, Pumpkin, Troll, Demon };
+    private string EnemyName;
+    private int EnemyHealthbar;
+    
+
+    void Start()
+    {
+       // sprites = Resources.LoadAll<Sprite>(Goblin, Pumpkin, Troll, Demon);
+
+        Enemies ThisEnemy;
+
+        ThisEnemy = Enemies.Goblin;
+    }
+
+    Enemies GetEnemy(Enemies Enemy)
+    {
+
+        if (Enemy == Enemies.Goblin)
+        {
+            EnemyHealthbar = 25;
+            //this.GetComponent<SpriteRenderer>().sprite = EnemyArt.Goblin;
+        }
+        else if (Enemy == Enemies.Pumpkin)
+        {
+            EnemyHealthbar = 15;
+        }
+        else if (Enemy == Enemies.Troll)
+        {
+            EnemyHealthbar = 50;
+        }
+        else if (Enemy == Enemies.Demon)
+        {
+            EnemyHealthbar = 30;
+        }
+
+        return Enemy;
+    }
+}
---
+++ Tower Defence/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] waypoints_1;
+
+    [SerializeField]
+    Transform[] waypoints_2;
+    
+    [SerializeField]
+    Transform[] Fullwaypoint;
+
+    float enemyMoveSpeed = 0.1f;
+    int waypointIndex = 0;
+    int currentWaypoint;
+
+    void Start () {
+        currentWaypoint = Random.Range(1, 3);
+
+        if (currentWaypoint == 1) {
+            Fullwaypoint = waypoints_1;
+            transform.position = Fullwaypoint[waypointIndex].transform.position;
+        }
+
+        if (currentWaypoint == 2)
+        {
+            Fullwaypoint = waypoints_2;
+            transform.position = Fullwaypoint[waypointIndex].transform.position;
+        }
+        waypointIndex += 1;
+    }
+	
+	void Update () {
+
+        Move();
+        TowerReach();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Fullwaypoint[waypointIndex].transform.position, enemyMoveSpeed * Time.deltaTime);
+
+        if (this.transform.position.x == Fullwaypoint[waypointIndex].transform.position.x
+            && this.transform.position.y == Fullwaypoint[waypointIndex].transform.position.y)
+        {
+            waypointIndex += 1;
+        }
+
+        Vector3 LookAtPosition = (Fullwaypoint[waypointIndex].transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(LookAtPosition.y, LookAtPosition.x) * Mathf.Rad2Deg;
+        Quaternion rotation = new Quaternion();
+        rotation.eulerAngles = new Vector3(0, 0, angle + 90);
+        transform.rotation = rotation;
+    }
+
+    void TowerReach()
+    {
+        if (waypointIndex == Fullwaypoint.Length)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
